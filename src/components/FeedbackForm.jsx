a3d551a2127a94d0ffa16084fbf19f04f1509d7e
Gieveppix <84123911+Gieveppix@users.noreply.github.com>
2022-03-08 18:37:34 +0100

    Route adjustment

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,7 @@ import Card from './shared/Card'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
@@ -13,32 +13,44 @@ function FeedbackForm() {
     const handleTextChange = (e) => { //event
         if(text === '') {
             setBtnDisabled(true)
-            setMessage('')
-        } else if(text !== '' && text.trim().length <= 9) {
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 8) {
             setMessage('Text must be at least 10 characters long')
             setBtnDisabled(true)
         } else {
-            setMessage('')
+            setMessage(null)
             setBtnDisabled(false)
         }
         setText(e.target.value)
     }
 
-  return (
-    <Card>
-        <form>
-            <h2>
-                How would you rate your service with us?
-            </h2>
-            <RatingSelect select={(rating) => setRating(rating)} />
-            <div className='input-group'>
-                <input onChange={handleTextChange} type='text' placeholder='Write a review' />
-                <Button type='submit' isDisabled={btnDisabled}>Send</Button>
-            </div>
-            {message && <div className='message'>{message}</div>}
-        </form>
-    </Card>
-  )
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 8) {
+            const newFeedback = {
+                text: text,
+                rating
+            }
+            handleAdd(newFeedback)
+            setText('') //Doesnt work
+        }
+    }
+
+    return (
+        <Card>
+            <form onSubmit={handleSubmit}>
+                <h2>
+                    How would you rate your service with us?
+                </h2>
+                <RatingSelect select={(rating) => setRating(rating)} />
+                <div className='input-group'>
+                    <input onChange={handleTextChange} type='text' placeholder='Write a review' />
+                    <Button type='submit' isDisabled={btnDisabled}>Send</Button>
+                </div>
+                {message && <div className='message'>{message}</div>}
+            </form>
+        </Card>
+    )
 }
 
 export default FeedbackForm
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
 function FeedbackList({ feedback, handleDelete }) {
-    if(!feedback || feedback.lenght === 0) {
+    if(!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>
     }
 
-
     return (
         <div className='feedback-List'>
+            <AnimatePresence>
             {feedback.map((item) => (
+                <motion.div
+                    key={item.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                >
                 <FeedbackItem 
                     key={item.id} item={item} 
                     handleDelete={handleDelete} />
+                    </motion.div>
             ))}
+            </AnimatePresence>
         </div>
     )
+
+    // return (
+    //     <div className='feedback-List'>
+    //         {feedback.map((item) => (
+    //             <FeedbackItem 
+    //                 key={item.id} item={item} 
+    //                 handleDelete={handleDelete} />
+    //         ))}
+    //     </div>
+    // )
 }
 
 FeedbackList.propTypes = {
     feedback: PropTypes.arrayOf(
         PropTypes.shape({
-        id: PropTypes.number.isRequired,
+        id: PropTypes.string.isRequired, //changed because of uuid
         text: PropTypes.string.isRequired,
         rating: PropTypes.number.isRequired,
         })
